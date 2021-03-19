72112606b132243a3e0aec56bce7554a0988fb26
unknown <alvaromendozaola@outlook.com>
2021-03-19 18:59:13 -0400

    Added Code

--- stock_price_MC_simulation.py
+++ stock_price_MC_simulation.py
@@ -20,15 +20,15 @@ def get_simulation(ticker, name):
     var = log_returns.var()
     drift = mean - (0.5 * var)
     stdev = log_returns.std()
-    t_intervals = 362 #adjust this value depending on the length of values the code from the "web.DataReader" is able to collect, it varies with the date
+    t_intervals = 368 #adjust this value depending on the length of values the code from the "web.DataReader" is able to collect, it varies with the date
     iterations = 10 #this will determine the number of simlations we are going to run. Can be more than 10
     daily_returns = np.exp(drift.values + stdev.values * norm.ppf(np.random.rand(t_intervals, iterations)))
     s0 = data.iloc[-1]
     price_list = np.zeros_like(daily_returns)
     price_list[0] = s0
-    for t in range(1, t_intervals): #we create the lists that contain the simlations created
+    for t in range(1, t_intervals): #creates the lists that contain the simlations created
         price_list[t] = price_list[t-1] * daily_returns[t]
-    #we then plot our simulation
+    #plot the simulation
     plt.figure(figsize = (10,6))
     plt.title("1 Year Monte Carlo Similation for " + name)
     plt.ylabel("Price (P)")
